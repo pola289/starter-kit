5b1e7777e53f920c6ce0a6fed3cedb76ee4bf363
pola289 <pola289lp@gmail.com>
2024-12-10 14:37:06 +0700

    Update next.config.js

--- packages/blog-starter-kit/themes/personal/next.config.js
+++ packages/blog-starter-kit/themes/personal/next.config.js
@@ -73,18 +73,7 @@ const config = {
 			},
 		],
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/ping/data-event',
-				destination: `${ANALYTICS_BASE_URL}/api/data-event`,
-			},
-			{
-				source: '/api/analytics',
-				destination: `${HASHNODE_ADVANCED_ANALYTICS_URL}/api/analytics`,
-			},
-		];
-	},
+  
 	async redirects() {
 		return await getRedirectionRules();
 	},
